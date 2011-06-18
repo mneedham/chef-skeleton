9fc805b49cb1577f7d7fc3e607596f062c6bc56e
Mark Needham <m.h.needham@gmail.com>
2011-06-18 06:11:36 -0700

    Edited setup.sh via GitHub

--- setup.sh
+++ setup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sudo yum install ruby-1.8.7.334-2.el5 ruby-devel-1.8.7.334-2.el5 ruby-ri-1.8.7.334-2.el5 ruby-rdoc-1.8.7.334-2.el5 git gcc gcc-c++ automake autoconf make
+yum install -y gcc openssl-devel zlib-devel.x86_64
+yum groupinstall -y development-tools development-libs
+
+cd /usr/local/src/
+wget ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
+tar xvzf ruby-1.9.2-p180.tar.gz
+cd ruby-1.9.2-p180
+./configure
+make && make install
+
+ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path 
+ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path 
+
+cd -
+
+rm /usr/local/src/ruby-1.9.2-p180.tar.gz
+rm -rf /usr/local/src/ruby-1.9.2-p180
+
+sudo gem install chef --no-rdoc --no-ri
