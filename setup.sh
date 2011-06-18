0a48c731395a9207f54c0f931202865f34708284
Mark Needham <m.h.needham@gmail.com>
2011-06-18 06:05:11 -0700

    Edited setup.sh via GitHub

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo rpm -Uvh http://download.fedora.redhat.com/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm
-
 sudo yum install ruby-1.8.7.334-2.el5 ruby-devel-1.8.7.334-2.el5 ruby-ri-1.8.7.334-2.el5 ruby-rdoc-1.8.7.334-2.el5 git gcc gcc-c++ automake autoconf make
