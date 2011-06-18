d2f3129065975647700f8465b68eaba4f948c1da
Mark Needham <m.h.needham@gmail.com>
2011-06-18 06:22:01 -0700

    Edited setup.sh via GitHub

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@ sudo yum install -y gcc openssl-devel zlib-devel.x86_64
 sudo yum groupinstall -y development-tools development-libs
 
 cd /usr/local/src/
-curl -O ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
-tar xvzf ruby-1.9.2-p180.tar.gz
+sudo curl -O ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
+sudo tar xvzf ruby-1.9.2-p180.tar.gz
 cd ruby-1.9.2-p180
-./configure
-make && make install
+sudo ./configure
+sudo make && sudo make install
 
-ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path 
-ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path 
+sudo ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path 
+sudo ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path 
 
 cd -
 
-rm /usr/local/src/ruby-1.9.2-p180.tar.gz
-rm -rf /usr/local/src/ruby-1.9.2-p180
+sudo rm /usr/local/src/ruby-1.9.2-p180.tar.gz
+sudo rm -rf /usr/local/src/ruby-1.9.2-p180
 
 sudo gem install chef --no-rdoc --no-ri
