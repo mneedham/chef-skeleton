7712ff906f12aa2ae98155ec232d130f7e426656
Casper <casper@mark-fedora.(none)>
2011-06-20 00:02:17 +0100

    mark: pulled out server name into an attribute. Also creating app server

---
+++ cookbooks/marklogic/attributes/default.rb
@@ -0,0 +1,1 @@
+default[:mark_logic][:server_name] = 'http://localhost:8001'
--- cookbooks/marklogic/recipes/default.rb
+++ cookbooks/marklogic/recipes/default.rb
@@ -42,12 +42,14 @@ end
 python "configure mark logic" do
   user "root"
   cwd "/tmp"
-  environment ({"PYTHONPATH" => '/usr/local/bin'})
+  environment ({"PYTHONPATH" => '/usr/local/bin', "server_name" => node[:mark_logic][:server_name]})
 
   code <<-EOH
 import booster
+import os
 
-server_name = "http://localhost:8001"
+server_name = os.getenv("server_name")
+
 booster.configureAuthHttpProcess(server_name, "admin", "admin")
 booster.license(server_name)
 booster.agree_go(server_name)
@@ -62,25 +64,51 @@ end
 python "configure mark logic" do
   user "root"
   cwd "/tmp"
-  environment ({"PYTHONPATH" => '/usr/local/bin'})
+  environment ({"PYTHONPATH" => '/usr/local/bin', "server_name" => node[:mark_logic][:server_name]})
 
   code <<-EOH
 import booster
+import os
 
-server_name = "http://localhost:8001"
+server_name = os.getenv("server_name")
+
 booster.configureAuthHttpProcess(server_name, "admin", "admin")
 booster.security_install_go(server_name)
 booster.test_admin_connection(server_name)
   EOH
 end
 
+python "teardown existing app server" do
+  user "root"
+  cwd "/tmp"
+  environment ({"PYTHONPATH" => '/usr/local/bin', "server_name" => node[:mark_logic][:server_name]})
+  only_if "curl -s --digest -u'admin:adgemin' http://localhost:9997"
+  code <<-EOH
+import booster
+import os
+
+print "Tearing down existing app server"
+
+server_name = os.getenv("server_name")
+
+booster.configureAuthHttpProcess(server_name, "admin", "admin")
+booster.booster(server_name, { "action":"appserver-delete", "appserver-name": 'http-9997', "group-name": "Default"})
+  EOH
+end
+
 python "setup app server" do
   user "root"
   cwd "/tmp"
-  environment ({"PYTHONPATH" => '/usr/local/bin'})
+  environment ({"PYTHONPATH" => '/usr/local/bin', "server_name" => node[:mark_logic][:server_name]})
   code <<-EOH
 import booster
+import os
 
+print "creating new app server"
+
+server_name = os.getenv("server_name")
+
+booster.configureAuthHttpProcess(server_name, "admin", "admin")
 booster.booster(server_name, { "action":"appserver-create-http", "group-name":"Default", "root": "/home/casper/casper/core/src/main/xquery",                               
 			       "database-name": 'Documents', "modules-name": 'file-system', "appserver-name": 'http-9997', "port": '9997'  })
 booster.booster(server_name, { "action":"appserver-set", "appserver-name": 'http-9997', "group-name": "Default",  "setting": "authentication", "value": "application-level"})
