e061a85ea33375161d591c773afb97d39d982d04
Mark Needham <m.h.needham@gmail.com>
2011-06-19 19:19:58 +0100

    mark: adding the /etc/chef files

---
+++ etc_chef/solo.rb
@@ -0,0 +1,4 @@
+file_cache_path "/home/casper/casper/chef-solo"
+cookbook_path "/home/casper/casper/chef-solo/cookbooks"
+json_attribs "/etc/chef/node.json"
+role_path "/home/casper/casper/chef-solo/roles"
